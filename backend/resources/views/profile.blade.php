18d6c573774057d634f2a22e482335bc5f1974f1
immanuel caneca <immancaneca@gmail.com>
2023-07-04 05:18:04 +0800

    fix line

--- backend/resources/views/profile.blade.php
+++ backend/resources/views/profile.blade.php
@@ -416,9 +416,7 @@
                     @endif
                 @endif
 
-                
-                <br>
-                <div class="btn-group dropstart more-btn">
+                <div class="btn-group dropstart more-btn mt-5">
                     <button type="button" title="Option" data-bs-toggle="dropdown" aria-expanded="false" style="background: var(--icon-color);">
                         <i class="fa-solid fa-ellipsis"></i>
                     </button>
@@ -736,5 +734,7 @@
 
         // To hide the friend request confirmation 
 
-    </script>
+</script>
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
 @endsection

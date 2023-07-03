42a0384ced681b23018e301300881b331c52bd77
immanuel caneca <immancaneca@gmail.com>
2023-07-03 12:40:50 +0800

    edit icon bg

--- backend/resources/views/profile.blade.php
+++ backend/resources/views/profile.blade.php
@@ -41,7 +41,7 @@
 
     .profile-btn {
         border-radius: 50%;
-        background: var(--body-color);
+        background: var(--bg-color);
         padding: 2px 7px;
         border: none;
         position: relative;
@@ -289,7 +289,7 @@
             <!-- Modal -->
             <div class="modal fade" id="cover_modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content" style="background: var(--bg-color);">
+                <div class="modal-content">
                     <div class="modal-header">
                     <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Cover Photo</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
